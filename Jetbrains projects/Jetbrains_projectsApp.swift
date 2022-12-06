d42a793cc5899c367edd6d94d2a51546f5339b69
mongi.zaidi <mongi.zaidi@glide-mobility.com>
2022-12-06 13:48:53 +0100

    Initial Commit

---
+++ Jetbrains projects/Jetbrains_projectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jetbrains_projectsApp.swift
+//  Jetbrains projects
+//
+//  Created by user on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Jetbrains_projectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Jetbrains projectsUITests/Jetbrains_projectsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Jetbrains_projectsUITestsLaunchTests.swift
+//  Jetbrains projectsUITests
+//
+//  Created by user on 06/12/2022.
+//
+
+import XCTest
+
+final class Jetbrains_projectsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
